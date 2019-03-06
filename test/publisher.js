29006a8fa5bdefc03ca8ff986a6c3553a954aaf2
thuyhangle <thuyhangle.le@gmail.com>
2019-03-06 04:34:13 +0200

    Add key

---
+++ test/publisher.js
@@ -0,0 +1,6 @@
+
+var pubnub = new PubNub({
+    publishKey:   'pub-c-963342c0-7b70-468e-aada-84a86ad5d532',
+    subscribeKey: 'pub-c-963342c0-7b70-468e-aada-84a86ad5d532'
+  });
+var xhr = new XMLHttpRequest();
