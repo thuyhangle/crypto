8000a5877c4b51b81ffc506c9f7ab01ca172c9ec
thuyhangle <thuyhangle.le@gmail.com>
2019-03-07 04:45:39 +0200

    Process publish request

--- test/publisher.js
+++ test/publisher.js
@@ -3,4 +3,44 @@ var pubnub = new PubNub({
     publishKey:   'pub-c-963342c0-7b70-468e-aada-84a86ad5d532',
     subscribeKey: 'pub-c-963342c0-7b70-468e-aada-84a86ad5d532'
   });
+
 var xhr = new XMLHttpRequest();
+
+function processRequest(e) {
+if (xhr.readyState == 4 && xhr.status == 200) {
+    var response = JSON.parse(xhr.responseText);
+        pubnub.publish({
+        channel: 'bitcoin-feed',
+        message: {
+            eon: {
+            'BitCoin': response.BTC.USD.toFixed(2)
+            }
+        }
+        });
+        pubnub.publish({
+        channel: 'ether-feed',
+        message: {
+            eon: {
+            'Ether': response.ETH.USD.toFixed(2)
+            }
+        }
+        });
+        pubnub.publish({
+        channel: 'litecoin-feed',
+        message: {
+            eon: {
+            'LiteCoin': response.LTC.USD.toFixed(2)
+            }
+        }
+        });
+    }
+}
+
+function mainApp() {
+    setInterval(function(){
+        xhr.open('GET', 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD', true)
+        xhr.send();
+        xhr.onreadystatechange = processRequest;
+    }, 10000)
+};
+mainApp();
