e690e861c49188161a3aeb310536473e1aa5c263
thuyhangle <thuyhangle.le@gmail.com>
2019-03-10 02:51:38 +0200

    Set up var for charts

--- test/subscriber.js
+++ test/subscriber.js
@@ -1,3 +1,10 @@
 var pubnub = new PubNub({
     subscribeKey: 'sub-c-96b25e44-3fb7-11e9-99ed-dea01fda7778'
 });
+
+var pointLimit = 15; //Total amount of points on your graph.
+var topPadding = 100; // Additional room between the top most point and the top of the chart
+var bottomPadding = 100; // Additional room for the bottom of the graph
+var eonData = {labels: true,type: 'line'}; // flag for rendering labels with the point and the type of line that will be rendered
+var eonAxis = {y: {padding: {top:topPadding, bottom:bottomPadding}},
+               x: {type: 'timeseries',tick: {format: '%H:%M:%S'}}}; // Formatting the time stamps along the bottom of the graphs.
