e9c2519533d43578d7d4dfacc154c85a1a551489
thuyhangle <thuyhangle.le@gmail.com>
2019-03-12 00:33:56 +0200

    Create charts

--- test/subscriber.js
+++ test/subscriber.js
@@ -8,3 +8,43 @@ var bottomPadding = 100; // Additional room for the bottom of the graph
 var eonData = {labels: true,type: 'line'}; // flag for rendering labels with the point and the type of line that will be rendered
 var eonAxis = {y: {padding: {top:topPadding, bottom:bottomPadding}},
                x: {type: 'timeseries',tick: {format: '%H:%M:%S'}}}; // Formatting the time stamps along the bottom of the graphs.
+
+// Create the EON Chart for BitCoin and bind its div
+eon.chart({
+channels: ['bitcoin-feed'],
+history: true,
+flow: true,
+limit: pointLimit,
+pubnub: pubnub,
+generate: {
+    bindto: '#bitcoinChart',
+    data: eonData,
+    axis: eonAxis
+}
+});
+// Create the Ether Chart for BitCoin and bind its div
+eon.chart({
+channels: ['ether-feed'],
+history: true,
+flow: true,
+limit: pointLimit,
+pubnub: pubnub,
+generate: {
+    bindto: '#etherChart',
+    data: eonData,
+    axis: eonAxis
+}
+});
+// Create the LiteCoin Chart for BitCoin and bind its div
+eon.chart({
+channels: ['litecoin-feed'],
+history: true,
+flow: true,
+limit: pointLimit,
+pubnub: pubnub,
+generate: {
+    bindto: '#liteCoinChart',
+    data: eonData,
+    axis: eonAxis
+}
+});
